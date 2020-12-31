36a412f786c5493bb5895dde7500256f7a30886b
smooti <66232763+smooti@users.noreply.github.com>
2020-12-30 21:46:59 -0500

    Uploading start to changing multiple users passwords

---
+++ Set-LocalPassword.ps1
@@ -0,0 +1,19 @@
+$Users = @()
+$ExistingUsers = @((Get-LocalUser | Select-Object Name).Name)
+$key = (1)
+$SecureString = Get-Content "<MyPasswordFile>"
+$password = ConvertTo-SecureString $SecureString -Key $key
+
+Try {
+    ForEach ($U in $Users) {
+        ForEach ($E in $ExistingUsers) {
+            if ($U -eq $E) {
+                Set-LocalUser -Name $U -Password $password
+                Write-Host "Success:`n`tUser - $($U)`n`tPassword has been changed.`n" -ForegroundColor Cyan
+            }
+        }
+    }
+}
+Catch {
+    Write-Host "Error: $($_.Exception.Message)" -ForegroundColor Red -BackgroundColor Black
+}
