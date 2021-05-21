f35296a7898153f732547c56826f74393b10a58f
smooti <66232763+smooti@users.noreply.github.com>
2021-05-21 08:30:20 -0400

    Changing way to process password and Usernames

--- Set-LocalPassword.ps1
+++ Set-LocalPassword.ps1
@@ -1,15 +1,36 @@
+<#
+    .Synopsis
+        Will use templates to create new checklists for all machines specified
+    .NOTES
+        Created by: Robert Owens
+        Date: December 31, 2020
+    .PARAMETER Destination
+        Destination where the results will go       
+    .EXAMPLE
+        .\Copy-Checklist -Destination "C:\ProgramData\ChecklistResults"
+#>
+
 #Requires -Version 5.1
-$Users = @()
+param (
+    [Parameter (
+        Mandatory = $True
+        )
+    [string[]]$Users,
+    [Parameter (
+        Mandatory = $True
+        )
+    [string]$Password = Read-Host "Password: " -AsSecureString
+    )
+    
 $ExistingUsers = @((Get-LocalUser | Select-Object Name).Name)
 $key = (1)
-$SecureString = Get-Content "<MyPasswordFile>"
 $password = ConvertTo-SecureString $SecureString -Key $key
 
 Try {
     ForEach ($U in $Users) {
         ForEach ($E in $ExistingUsers) {
             if ($U -eq $E) {
-                Set-LocalUser -Name $U -Password $password
+                Set-LocalUser -Name $U -Password $Password
                 Write-Host "Success:`n`tUser - $($U)`n`tPassword has been changed.`n" -ForegroundColor Cyan
             }
         }
